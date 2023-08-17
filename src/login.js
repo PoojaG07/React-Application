c82db0616e89d40a7fccd18f2f043dc4e1e40d84
Gollapally Pooja <gollapally.pooja@globallogic.com>
2023-08-17 20:26:33 +0530

    react application files

---
+++ src/login.js
@@ -0,0 +1,17 @@
+export function Login(){
+return(
+    <div>
+       <h1>
+        User Login
+       </h1>
+       <dl>
+        <dt>UserName</dt>
+        <dd><input type="text" /></dd>
+        <dt>Password</dt>
+        <dd><input type="password" /></dd>
+       </dl>
+       <button>login</button>
+    </div>
+
+)
+}
